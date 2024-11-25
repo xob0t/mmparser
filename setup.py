2ffac2552df8802f4930d0889c14b6b701993dcf
xob0t <5348245@gmail.com>
2024-11-25 13:56:10 +0300

    chore: version bump

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mmparser',
-    version='0.6.3',
-    description='Парсер megamarket.ru',
-    author='xob0t',
-    url='https://github.com/xob0t/mmparser',
+    name="mmparser",
+    version="0.6.5",
+    description="Парсер megamarket.ru",
+    author="xob0t",
+    url="https://github.com/xob0t/mmparser",
     packages=find_packages(),
     install_requires=[
-        'rich',
-        'curl-cffi',
-        'InquirerPy',
+        "rich",
+        "curl-cffi",
+        "InquirerPy",
     ],
     extras_require={
-        'lxml': ['lxml'],
+        "lxml": ["lxml"],
     },
-    entry_points={
-        'console_scripts': ['mmparser = core.main:main']
-    },
+    entry_points={"console_scripts": ["mmparser = core.main:main"]},
 )
