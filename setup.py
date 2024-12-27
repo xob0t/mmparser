07097b904e953008240b56fdc1df2833c24dfc2b
xob0t <5348245@gmail.com>
2024-12-27 07:12:10 +0300

    chore: version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mmparser",
-    version="0.9.5",
+    version="0.9.6",
     description="Парсер megamarket.ru",
     author="xob0t",
     url="https://github.com/xob0t/mmparser",
